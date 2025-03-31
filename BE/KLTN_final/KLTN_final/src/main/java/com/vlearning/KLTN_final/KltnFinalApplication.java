b35a4b3b145767e23757ca344c1cc0cc698056ae
nhtfw <huuthangfw@gmail.com>
2025-03-31 22:28:15 +0700

    khoi tao du an BE

---
+++ BE/KLTN_final/KLTN_final/src/main/java/com/vlearning/KLTN_final/KltnFinalApplication.java
@@ -0,0 +1,13 @@
+package com.vlearning.KLTN_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KltnFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KltnFinalApplication.class, args);
+	}
+
+}
---
+++ BE/KLTN_final/KLTN_final/src/test/java/com/vlearning/KLTN_final/KltnFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vlearning.KLTN_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KltnFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
